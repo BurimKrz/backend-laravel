e91c26b443d4ddbf1c1566a374c050ace9eb9a88
ElsaRamadani <125298497+ElsaRamadani@users.noreply.github.com>
2023-05-09 11:22:16 +0200

    Import/Export

--- app/Http/Controllers/ImportPoduct.php
+++ app/Http/Controllers/ImportPoduct.php
@@ -31,7 +31,7 @@ class ImportPoduct extends Controller
            ->join('product as p', 'imp.product_id', '=', 'p.id')
            ->join('company as c', 'c.id', '=', 'p.company_id')
            ->join('product_category as pc', 'pc.id', '=', 'p.category_id')
-           ->where('imp.id', '=', $id)
+           ->where('imp.product_id', '=', $id)
            ->select('p.name', 'p.description', 'p.price', 'p.imageURL', 'p.views', 'c.name as company_name', 'c.country', 'c.keywords', 'pc.name as category_name')
            ->get();
             $array = $importProducts->map(function ($obj) {
