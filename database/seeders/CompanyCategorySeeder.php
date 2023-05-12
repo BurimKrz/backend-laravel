991d5b3068981ed712c6ff9ba0fdcd3d341f6a74
GERI12326 <114013384+GERI12326@users.noreply.github.com>
2023-05-12 08:28:29 +0200

    commiting seeders

--- database/seeders/CompanyCategorySeeder.php
+++ database/seeders/CompanyCategorySeeder.php
@@ -26,3 +26,5 @@ class CompanyCategorySeeder extends Seeder
                 company_category::create($category);
             }
 }
+}
+}
--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -12,51 +12,17 @@ class ProductCategorySeeder extends Seeder
     /**
      * Run the database seeds.
      */
-<<<<<<< HEAD
 
-=======
->>>>>>> feat/UC13_categorize_companies
     public function run(): void
     {
         DB::table('product_category') -> insert([
             ['name' => 'Fashion'],
             ['name' => 'Home'],
-<<<<<<< HEAD
             ['name' => 'Accessories'],
-=======
-            ['name' => 'Acessories'],
->>>>>>> feat/UC13_categorize_companies
             ['name' => 'Sporting'],
             ['name' => 'Health'],
             ['name' => 'Medical'],
             ['name' => 'Pets'],
-<<<<<<< HEAD
-            ['name' => 'Fashion'],
-
         ]);
     }
-=======
-        ]);
-    }
-
-//     public function run(): void
-// {
-//     $categories = [
-//         ['name' => 'Fashion'],
-//         ['name' => 'Home'],
-//         ['name' => 'Acessories'],
-//         ['name' => 'Sporting'],
-//         ['name' => 'Health'],
-//         ['name' => 'Medical'],
-//         ['name' => 'Pets'],
-//     ];
-
-//     foreach ($categories as $category) {
-//         if (!DB::table('product_category')->where('name', $category['name'])->exists()) {
-//             DB::table('product_category')->insert($category);
-//         }
-//     }
-// }
-
->>>>>>> feat/UC13_categorize_companies
 }
