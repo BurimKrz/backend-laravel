d724a61e48f11f981194c245c454066eecd8c80f
Bumirec <bumirec@gmail.com>
2023-05-15 10:33:53 +0200

    company category bug fix

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -16,7 +16,7 @@ class CategoryResource extends JsonResource
     {
         return [
             'id'       => $this->id,
-            'category' => $this->category,
+            'name' => $this->name,
 
         ];
     }
