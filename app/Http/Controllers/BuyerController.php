df97086514f93fb928e6fc0f3f794100887e9386
Ermira Bajrami <ermirabajrami91@gmail.com>
2023-05-18 15:42:46 +0200

    test

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -6,28 +6,28 @@ use App\Models\buyerConfirmation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-
 class BuyerController extends Controller
 {
-    public function buyerConfirmation(Request $request){
-         
+    public function buyerConfirmation(Request $request)
+    {
+
         $validated = Validator::make(
-            $request -> all(),
+            $request->all(),
             [
-                'user_id' => 'required|integer',
-                'product_id' => 'required|integer',
-                'confirmation' => 'required|boolean:true, false'
+                'user_id'      => 'required|integer',
+                'product_id'   => 'required|integer',
+                'confirmation' => 'required|boolean:true, false',
             ]
         );
-        if($validated->fails()){
+        if ($validated->fails()) {
             return response()->json(['errors' => $validated->errors()], 400);
         }
 
         $buyer = BuyerConfirmation::create(
             [
-                'user_id' => $request->user_id,
-                'product_id' => $request->product_id,
-                'confirmation' => $request->confirmation
+                'user_id'      => $request->user_id,
+                'product_id'   => $request->product_id,
+                'confirmation' => $request->confirmation,
             ]
         );
         return response()->json(['buyer' => $buyer], 201);
