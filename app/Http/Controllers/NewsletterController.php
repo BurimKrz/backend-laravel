a7c3ea7c50d79d7b111616b00cf99041b59725fd
ElsaRamadani <125298497+ElsaRamadani@users.noreply.github.com>
2023-05-29 09:45:38 +0200

    Newsletter

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Newsletter;
 use App\Models\newsletters;
-use App\Notifications\WelcomeMailNotifitacion;
-use App\Mail\WelcomeMail;
-use App\Mail\NewsletterMail;
-use Illuminate\Support\Facades\Notification;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
     public function addNewsletter(Newsletter $request)
     {
-        $newsletter = Newsletters::create([
-            'email' => $request->email,
-        ]);
+        $count = Newsletters::where('email', $request->email)->count();
 
-        return response()->json(['newsletter' => $newsletter], 201);
+        if ($count === 1) {
+            return response()->json(['error' => 'Email already exists'], 400);
+        } else {
+            try {
+                $newsletter = Newsletters::create([
+                    'email' => $request->email,
+                ]);
+        
+                return response()->json(['newsletter' => $newsletter], 201);
+            } catch (QueryException $e) {
+                return response()->json(['error' => 'Something went wrong'], 500);
+            }
+        }
     }
 
     public function sendNewsletter(Request $request)
@@ -28,7 +33,7 @@ class NewsletterController extends Controller
         // $newsletterEmails = Newsletters::pluck('email')->all();
         // $subject = $request->input('subject');
         // $message = $request->input('message');
-    
+
         // foreach ($newsletterEmails as $email) {
         //     Mail::raw($message, function ($m) use ($email, $subject) {
         //         $m->to($email)->subject($subject);
