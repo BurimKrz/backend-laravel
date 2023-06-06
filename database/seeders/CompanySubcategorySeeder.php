5043f662c3614fa9594d5a34549be1dd71a87c08
GERI12326 <114013384+GERI12326@users.noreply.github.com>
2023-06-06 09:28:46 +0200

    test

--- database/seeders/CompanySubcategorySeeder.php
+++ database/seeders/CompanySubcategorySeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ProductSubcategory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class CompanySubCategorySeeder extends Seeder
 {
@@ -13,16 +12,16 @@ class CompanySubCategorySeeder extends Seeder
      */
     public function run(): void
     {
-          DB::table('company_subcategories') -> insert([
-              ['name' => 'Limited by Sheares', 'category_id' => 1],
+        ProductSubcategory::insert([
+            ['name' => 'Limited by Sheares', 'category_id' => 1],
             ['name' => 'Limited by Guarante', 'category_id' => 1],
             ['name' => 'Unlimited Company', 'category_id' => 1],
             ['name' => 'Holding', 'category_id' => 2],
             ['name' => 'Subsidiary', 'category_id' => 2],
             ['name' => 'Assosiate', 'category_id' => 2],
             ['name' => 'Listed', 'category_id' => 3],
-            ['name' => 'Unlisted','category_id' => 3],
-            ['name' => 'Goverment','category_id' => 4],
+            ['name' => 'Unlisted', 'category_id' => 3],
+            ['name' => 'Goverment', 'category_id' => 4],
             ['name' => 'Foreing', 'category_id' => 4],
             ['name' => 'Section 8', 'category_id' => 4],
         ]);
